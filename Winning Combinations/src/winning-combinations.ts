0d3b3a6ec9c4ebadada0a911263e3eb44997d64f
jean <v-jean.gengnagel@definedcrowed.com>
2023-12-20 02:05:37 -0300

    comments

--- Winning Combinations/src/winning-combinations.ts
+++ Winning Combinations/src/winning-combinations.ts
@@ -1,39 +1,56 @@
+// Define the type for the result of winning combinations.
 type WinningCombinationsResult = [number, number[]][];
 
+// Function to identify winning combinations in a given array of symbols.
 function call(lines: number[]): WinningCombinationsResult {
+  // Define the paying symbols and the wild symbol.
   const payingSymbols = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   const wildSymbol = 0;
 
+  // Initialize an array to store the winning combinations.
   const result: WinningCombinationsResult = [];
 
+  // Iterate through the array to check for winning combinations.
   for (let i = 0; i <= lines.length - 3; i++) {
+    // Get the symbol at the current position.
     const symbol = lines[i];
 
+    // Check if the symbol is a paying symbol or the wild symbol.
     if (payingSymbols.includes(symbol) || symbol === wildSymbol) {
+      // Initialize an array to store the positions of the pay line.
       const payLine: number[] = [i];
       let j = i + 1;
 
+      // Continue checking for symbols in the pay line.
       while (
         j < lines.length &&
         (payingSymbols.includes(lines[j]) || lines[j] === wildSymbol)
       ) {
+        // Add the position to the pay line array.
         payLine.push(j);
         j++;
       }
 
+      // Check if the pay line has at least 3 symbols.
       if (payLine.length >= 3) {
-        const uniqueSymbol = Array.from(new Set(payLine.map((index) => lines[index])));
+        // Get the unique symbol at the starting position of the pay line.
+        const uniqueSymbol = lines[payLine[0]];
+
+        // Check if the unique symbol is a paying symbol or the wild symbol.
         if (
-          uniqueSymbol.length === 1 &&
-          (payingSymbols.includes(uniqueSymbol[0]) || uniqueSymbol[0] === wildSymbol)
+          payingSymbols.includes(uniqueSymbol) ||
+          uniqueSymbol === wildSymbol
         ) {
-          result.push([uniqueSymbol[0], payLine]);
+          // Add the winning combination to the result array.
+          result.push([uniqueSymbol, payLine]);
         }
       }
     }
   }
 
+  // Return the array of winning combinations.
   return result;
 }
 
+// Export the function as part of the WinningCombinations object.
 export const WinningCombinations = { call };
