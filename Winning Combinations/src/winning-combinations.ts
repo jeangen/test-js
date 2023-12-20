0e870afe8e4d8dfc6b979ee5e5c1f1339dbf4377
jean <v-jean.gengnagel@definedcrowed.com>
2023-12-20 02:04:17 -0300

    wiining combinations call function

--- Winning Combinations/src/winning-combinations.ts
+++ Winning Combinations/src/winning-combinations.ts
@@ -1,8 +1,39 @@
-
 type WinningCombinationsResult = [number, number[]][];
 
-
 function call(lines: number[]): WinningCombinationsResult {
-  return [[6, [2, 3, 4]]];
+  const payingSymbols = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const wildSymbol = 0;
+
+  const result: WinningCombinationsResult = [];
+
+  for (let i = 0; i <= lines.length - 3; i++) {
+    const symbol = lines[i];
+
+    if (payingSymbols.includes(symbol) || symbol === wildSymbol) {
+      const payLine: number[] = [i];
+      let j = i + 1;
+
+      while (
+        j < lines.length &&
+        (payingSymbols.includes(lines[j]) || lines[j] === wildSymbol)
+      ) {
+        payLine.push(j);
+        j++;
+      }
+
+      if (payLine.length >= 3) {
+        const uniqueSymbol = Array.from(new Set(payLine.map((index) => lines[index])));
+        if (
+          uniqueSymbol.length === 1 &&
+          (payingSymbols.includes(uniqueSymbol[0]) || uniqueSymbol[0] === wildSymbol)
+        ) {
+          result.push([uniqueSymbol[0], payLine]);
+        }
+      }
+    }
+  }
+
+  return result;
 }
+
 export const WinningCombinations = { call };
